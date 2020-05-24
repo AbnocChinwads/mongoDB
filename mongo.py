f9d9e2b9f9a67f7d3497f6f7de93e4bcd5799b7a
AbnocChinwads <rnemo@hotmail.co.uk>
2020-05-24 16:14:41 +0000

    Used CRUD to manipulate the data in the DB

--- mongo.py
+++ mongo.py
@@ -22,7 +22,10 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
+coll.update_many({'nationality': 'english'},
+                 {'$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'english'})
 
 for doc in documents:
     print(doc)
